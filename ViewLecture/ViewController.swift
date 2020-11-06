a7ecca5a4d27cf1b51ad2c9c7575e0eb87a66440
Stephan Markovski <smarkovski4271@stu.d214.org>
2020-11-06 12:08:07 -0800

    Initial Commit

---
+++ ViewLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewLecture
+//
+//  Created by user171629 on 11/6/20.
+//  Copyright © 2020 Stephan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
